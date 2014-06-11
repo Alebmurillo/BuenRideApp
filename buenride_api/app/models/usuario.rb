aef21b1c81bd894dfa39dc804435d0054e683995
luisrcarrillo <luisrcarrillo2205@gmail.com>
2014-06-11 09:27:15 -0600

    actualizacion al model

--- buenride_api/app/models/usuario.rb
+++ buenride_api/app/models/usuario.rb
@@ -1,2 +1,5 @@
 class Usuario < ActiveRecord::Base
+    has_many :reviews, dependent: :destroy
+    validates :email,:username, :password, :presence =>true
+    validates_uniqueness_of :username , :email
 end
