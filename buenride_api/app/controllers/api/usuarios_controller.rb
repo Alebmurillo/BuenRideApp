fb69ebd458f321787ab8ed15fda791faece0958e
luisrcarrillo <luisrcarrillo2205@gmail.com>
2014-06-11 09:23:54 -0600

    correccion al controller

--- buenride_api/app/controllers/api/usuarios_controller.rb
+++ buenride_api/app/controllers/api/usuarios_controller.rb
@@ -10,14 +10,8 @@ class UsuariosController < ApplicationController
     end  
   
     def index
-      usuarios_paginated= Usuario.order('id').page(params[:page]).per(PER_PAGE_RECORDS)
-      json_response={
-        models: usuarios_paginated,
-        current_page: params[:page].to_i,
-        perPage:PER_PAGE_RECORDS,
-        total_page: usuarios_paginated.num_pages
-      }
-      respond_with json_response     
+      usuarios = Usuario.order('id')
+      respond_with usuarios 
     end
     def new
       
