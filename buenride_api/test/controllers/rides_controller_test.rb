5bfcec99b66129cc1560a0706043c258a617a890
Luis <luisrcarrillo2205@gmail.com>
2014-06-11 11:18:57 -0600

    correccion api

--- buenride_api/test/controllers/rides_controller_test.rb
+++ buenride_api/test/controllers/rides_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PersonasControllerTest < ActionController::TestCase
+class RidesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
