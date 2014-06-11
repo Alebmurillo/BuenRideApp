138f605f6af42934db143ff86eb45e03b00012c9
tser91 <unknown-user@192.168.0.114>
2014-06-11 16:19:23 -0600

    autenticacion. agregar usuarios.

--- BuenRide/BuenRide.iPhone/MainViewController.designer.cs
+++ BuenRide/BuenRide.iPhone/MainViewController.designer.cs
@@ -14,9 +14,133 @@ namespace BuenRide.iPhone
 	[Register ("MainViewController")]
 	partial class MainViewController
 	{
-		[Action ("UIButton496_TouchUpInside:")]
+		[Outlet]
 		[GeneratedCode ("iOS Designer", "1.0")]
-		partial void UIButton496_TouchUpInside (UIButton sender);
+		UIButton AR_AddButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton AR_CancelButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton AR_CurrentLoc1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton AR_CurrentLoc2 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField AR_ObservTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton CRC_ByMap1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton CRC_Bymap2 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton CRC_CalculateCostButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton CRC_CurrentLoc1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton CRC_CurrentLoc2 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel CRC_DailyCostLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField CRC_DaysPerWeekTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField CRC_GasCostTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel CRC_MonthlyCostLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField CRC_Profite_TextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel CRC_WeeklyCostLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField LI_EmailTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField LI_PasswordTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton LI_SignInButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton LI_SignUpButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Sh_FbButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Sh_TwButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton SU_CancelButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField SU_EmailTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField SU_NameTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField SU_PasswordTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField SU_PhoneTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton SU_SignUpButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField SU_UsernameTextField { get; set; }
+
+		[Action ("AR_CurrentLoc1_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void AR_CurrentLoc1_TouchUpInside (UIButton sender);
+
+		[Action ("LI_SignInButton_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void LI_SignInButton_TouchUpInside (UIButton sender);
+
+		[Action ("SU_SignUpButton_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void SU_SignUpButton_TouchUpInside (UIButton sender);
 
 		[Action ("UIButton528_TouchUpInside:")]
 		[GeneratedCode ("iOS Designer", "1.0")]
@@ -28,6 +152,122 @@ namespace BuenRide.iPhone
 
 		void ReleaseDesignerOutlets ()
 		{
+			if (AR_AddButton != null) {
+				AR_AddButton.Dispose ();
+				AR_AddButton = null;
+			}
+			if (AR_CancelButton != null) {
+				AR_CancelButton.Dispose ();
+				AR_CancelButton = null;
+			}
+			if (AR_CurrentLoc1 != null) {
+				AR_CurrentLoc1.Dispose ();
+				AR_CurrentLoc1 = null;
+			}
+			if (AR_CurrentLoc2 != null) {
+				AR_CurrentLoc2.Dispose ();
+				AR_CurrentLoc2 = null;
+			}
+			if (AR_ObservTextField != null) {
+				AR_ObservTextField.Dispose ();
+				AR_ObservTextField = null;
+			}
+			if (CRC_ByMap1 != null) {
+				CRC_ByMap1.Dispose ();
+				CRC_ByMap1 = null;
+			}
+			if (CRC_Bymap2 != null) {
+				CRC_Bymap2.Dispose ();
+				CRC_Bymap2 = null;
+			}
+			if (CRC_CalculateCostButton != null) {
+				CRC_CalculateCostButton.Dispose ();
+				CRC_CalculateCostButton = null;
+			}
+			if (CRC_CurrentLoc1 != null) {
+				CRC_CurrentLoc1.Dispose ();
+				CRC_CurrentLoc1 = null;
+			}
+			if (CRC_CurrentLoc2 != null) {
+				CRC_CurrentLoc2.Dispose ();
+				CRC_CurrentLoc2 = null;
+			}
+			if (CRC_DailyCostLabel != null) {
+				CRC_DailyCostLabel.Dispose ();
+				CRC_DailyCostLabel = null;
+			}
+			if (CRC_DaysPerWeekTextField != null) {
+				CRC_DaysPerWeekTextField.Dispose ();
+				CRC_DaysPerWeekTextField = null;
+			}
+			if (CRC_GasCostTextField != null) {
+				CRC_GasCostTextField.Dispose ();
+				CRC_GasCostTextField = null;
+			}
+			if (CRC_MonthlyCostLabel != null) {
+				CRC_MonthlyCostLabel.Dispose ();
+				CRC_MonthlyCostLabel = null;
+			}
+			if (CRC_Profite_TextField != null) {
+				CRC_Profite_TextField.Dispose ();
+				CRC_Profite_TextField = null;
+			}
+			if (CRC_WeeklyCostLabel != null) {
+				CRC_WeeklyCostLabel.Dispose ();
+				CRC_WeeklyCostLabel = null;
+			}
+			if (LI_EmailTextField != null) {
+				LI_EmailTextField.Dispose ();
+				LI_EmailTextField = null;
+			}
+			if (LI_PasswordTextField != null) {
+				LI_PasswordTextField.Dispose ();
+				LI_PasswordTextField = null;
+			}
+			if (LI_SignInButton != null) {
+				LI_SignInButton.Dispose ();
+				LI_SignInButton = null;
+			}
+			if (LI_SignUpButton != null) {
+				LI_SignUpButton.Dispose ();
+				LI_SignUpButton = null;
+			}
+			if (Sh_FbButton != null) {
+				Sh_FbButton.Dispose ();
+				Sh_FbButton = null;
+			}
+			if (Sh_TwButton != null) {
+				Sh_TwButton.Dispose ();
+				Sh_TwButton = null;
+			}
+			if (SU_CancelButton != null) {
+				SU_CancelButton.Dispose ();
+				SU_CancelButton = null;
+			}
+			if (SU_EmailTextField != null) {
+				SU_EmailTextField.Dispose ();
+				SU_EmailTextField = null;
+			}
+			if (SU_NameTextField != null) {
+				SU_NameTextField.Dispose ();
+				SU_NameTextField = null;
+			}
+			if (SU_PasswordTextField != null) {
+				SU_PasswordTextField.Dispose ();
+				SU_PasswordTextField = null;
+			}
+			if (SU_PhoneTextField != null) {
+				SU_PhoneTextField.Dispose ();
+				SU_PhoneTextField = null;
+			}
+			if (SU_SignUpButton != null) {
+				SU_SignUpButton.Dispose ();
+				SU_SignUpButton = null;
+			}
+			if (SU_UsernameTextField != null) {
+				SU_UsernameTextField.Dispose ();
+				SU_UsernameTextField = null;
+			}
 		}
 	}
 }
